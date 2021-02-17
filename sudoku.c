e94138d381b4d489e995b9a5cdc5f56f482ecf2c
Raggdoll <Cozartkevin@gmail.com>
2021-02-16 20:54:26 -0800

    Validation and Experimentation
    Setup function for validating the initial 81 element array.  Setup bones of isGridValid with comments and pthread_Create example.

--- sudoku.c
+++ sudoku.c
@@ -44,13 +44,56 @@ int main(int argc, char *argv[])
 {
     parse_args(argc, argv);
 
+    pthread_t workerThreads[27];
+    int currentThread = 0; 
+
+
     if (verbose && use_fork) {
         printf("We are forking child processes as workers.\n");
+
+
     } else if (verbose) {
         printf("We are using worker threads.\n");
+
+        pthread_create(&workerThreads[currentThread++], NULL, isGridValid, infoStruct);  //example of how to call with thread for isGridValid. 
+
+
     }
 
 
     return 0;
 }
 
+bool validateIncommingArray(int inputArray[]) {
+
+    int n = sizeof(inputArray) / sizeof(inputArray[0]);  //Takes array and divides it by the first element to get the number of array elements. 
+
+    if (n == 81) { return true; }; // returns true if the array elements is 81, returns false (Invalid input) if not 81 elements. 
+    return false; 
+
+}
+
+void* isGridValid(void *infoStruct) {
+
+    int validArray[9] = 0; //Validation Array to check if number is proper. 
+
+    for (int i = row; i < row + 3; i++) {  //parse through row of information for 3 x 3 grid
+        for (int j = col; j < col + 3; j++) { //parse through Col of information for 3 x 3 grid
+
+            if (*What are we looking for here ?  ) { //If not valid entry in sudoku, Check if number is lower than 1 or greater than 9, then check if we already have that number. 
+
+                //If inside here, it isn't valid and we should flag it's col and row, return by closing thread as it wont hit the close thread at the end of the function. 
+                pthread_exit(NULL);
+            }
+            else {  //Is valid entry thus continues loop. 
+
+
+            }
+
+
+        }
+    }
+
+
+    pthread_exit(NULL);
+}
